e14846247b85127a40064478df3ccae2faaeec70
panku-chavan <pankajchavan963@gmail.com>
2023-03-14 22:05:13 +0530

    add backend

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express= require('express');
+
+const app=express();
+
+app.get('/',(req,res,next)=>{
+    res.send("hello")
+})
+
+
+module.exports=app;
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const mongoose=require('mongoose');
+require('dotenv').config();
+const app=require('./app');
+const port= 5000;
+
+const Database_url='mongodb://127.0.0.1:27017/test';
+
+mongoose.connect(Database_url,{useNewUrlParser: true,useUnifiedTopology: true });
+
+const db=mongoose.connection;
+db.on('error',(err)=>console.log(err));
+db.once('open',()=>console.log("Connected to database"));
+
+
+app.listen(port,()=>{
+    console.log("App listening on port",port);
+})
