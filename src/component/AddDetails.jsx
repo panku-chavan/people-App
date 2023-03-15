5f146d353636274326016693e2a04c50d23c4385
panku-chavan <pankajchavan963@gmail.com>
2023-03-15 18:55:05 +0530

    add details component modified

--- src/component/AddDetails.jsx
+++ src/component/AddDetails.jsx
@@ -2,25 +2,31 @@ import axios from "axios";
 import { useState } from "react";
 
 export const AddDetails = () => {
-    const [name, setName]=useState('');
-    const [email,setEmail]=useState('');
-    const [mobile,setMobile]=useState('');
-    const [dob,setDob]=useState('');
-    const [gender,setGender]=useState('');
-    const info={
-        name:name,
-        email:email,
-        mobile:mobile,
-        dob:dob,
-        gender,gender
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [dob, setDob] = useState('');
+    const [gender, setGender] = useState('');
+    const info = {
+        name: name,
+        email: email,
+        mobile: mobile,
+        dob: dob,
+        gender, gender
     }
-    const handleSubmit=(event)=>{
+    const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("form submitted",info);
 
-        axios.post('http://localhost:5000/peoples',info)
-        .then((res)=>console.log(res))
-        .catch((err)=>console.log(err))
+        if (name !== '' && email !== '' && mobile !== '' && dob !== '' && gender !== '') {
+            console.log("form submitted", info);
+            axios.post('http://localhost:5000/peoples', info)
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err))
+        }else{
+            
+        }
+
+
 
         setName('');
         setMobile('');
@@ -36,19 +42,19 @@ export const AddDetails = () => {
                 <div className="row d-flex justify-content-center">
                     <form action="" className=" col-md-6 " onSubmit={handleSubmit}>
                         <label className="form-label" htmlFor="">Name</label>
-                        <input className="form-control" type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)} />
+                        <input className="form-control" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
 
                         <label className="form-label" htmlFor="">Email</label>
-                        <input className="form-control" type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                        <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
                         <label className="form-label" htmlFor="">Mobile</label>
-                        <input className="form-control" type="number" value={mobile} onChange={(e)=>setMobile(e.target.value)}/>
+                        <input className="form-control" type="number" value={mobile} onChange={(e) => setMobile(e.target.value)} />
 
                         <label className="form-label" htmlFor="">Birth Date</label>
-                        <input className="form-control" type="date" value={dob} onChange={(e)=>setDob(e.target.value)}/>
+                        <input className="form-control" type="date" value={dob} onChange={(e) => setDob(e.target.value)} />
 
                         <label className="form-label" htmlFor="">Gender</label>
-                        <input className="form-control" type="text" value={gender} onChange={(e)=>setGender(e.target.value)}/>
+                        <input className="form-control" type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
 
                         <div className="row d-flex justify-content-center">
                             <input type="submit" className="btn btn-primary mt-4 col-md-2" value="Add" />
